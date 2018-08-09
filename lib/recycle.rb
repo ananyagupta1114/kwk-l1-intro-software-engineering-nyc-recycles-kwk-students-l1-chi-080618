376bba6e6018c03091054170b0ec2defb23aaefe
ananyagupta1114 <ananyagupta1114@gmail.com>
2018-08-09 16:51:24 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -58,5 +58,10 @@ all_data(first_recycle_bin)
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
 #You should use the `each` method to iterate over your hash to print out that sentence
 
-
+def key_and_data(first_recycle_bin)
+  first_recycle_bin.each do |key, value|
+   puts "The #{key} is #{value}."
+   end
+end
+key_and_data(first_recycle_bin)
 
